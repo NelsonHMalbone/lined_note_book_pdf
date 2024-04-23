2113109be3621a84fba83c20cb779b300220f1e0
Nelson Malbone <nhmalbone@icloud.com>
2024-04-23 18:06:35 -0400

    set up the under line for the title pages

--- note_book.py
+++ note_book.py
@@ -6,9 +6,11 @@ df = pd.read_csv('topics.csv')
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
 for index, rows in df.iterrows():
-    # getting the pdf set up
+    # getting the pdf set up # placed in forloop to get more pages # aka main parent
     pdf.add_page()
-    pdf.set_font(family='Times', style="B", size=12)
-    pdf.cell(w=0, h=12, txt=rows['Topic'], align='L', ln=1, border=1)
+    pdf.set_font(family='Times', style="B", size=24)
+    pdf.cell(w=0, h=24, txt=rows['Topic'], align='L', ln=1)
+    pdf.line(10, 28, 200, 28)
+
 
 pdf.output('lined_notepad.pdf')
