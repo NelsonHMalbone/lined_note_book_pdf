2d7441e8dce3d46c81fadd0494ece533aa8f5bad
Nelson Malbone <nhmalbone@icloud.com>
2024-04-23 18:01:13 -0400

    pdf now has a parent FOR LOOP for main body. in 39 pages for 39 different Topics

--- note_book.py
+++ note_book.py
@@ -1,9 +1,14 @@
 from fpdf import FPDF
+import pandas as pd
+# getting the csv file for data
+df = pd.read_csv('topics.csv')
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
-# getting the pdf set up
-pdf.add_page()
-pdf.set_font(family='Times', style="B", size=12)
-pdf.cell(w=0, h=12, txt='Welcome', align='L', ln=1, border=1)
+
+for index, rows in df.iterrows():
+    # getting the pdf set up
+    pdf.add_page()
+    pdf.set_font(family='Times', style="B", size=12)
+    pdf.cell(w=0, h=12, txt=rows['Topic'], align='L', ln=1, border=1)
 
 pdf.output('lined_notepad.pdf')
