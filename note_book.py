449858ed1a1c6130f9bba62cff967405926826bf
Nelson Malbone <nhmalbone@icloud.com>
2024-04-23 17:50:27 -0400

    rough code set up for creating pdf.

--- note_book.py
+++ note_book.py
@@ -1,3 +1,9 @@
 from fpdf import FPDF
 
-pdf = FPDF
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+# getting the pdf set up
+pdf.add_page()
+pdf.set_font(family='Times', style="B", size=12)
+pdf.cell(w=0, h=12, txt='Welcome', align='L', ln=1, border=1)
+
+pdf.output('lined_notepad.pdf')
